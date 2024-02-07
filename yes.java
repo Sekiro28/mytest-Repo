130a91fe62d3c1a22cd63ebf85021343595109ca
Harshit <108220156+Sekiro28@users.noreply.github.com>
2024-02-07 09:02:06 +0530

    Update yes.java

--- yes.java
+++ yes.java
@@ -6,15 +6,9 @@ class Codechef
 {
 	public static void main (String[] args) throws java.lang.Exception
 	{
-		Scanner read = new Scanner(System.in);
-		int N = read.nextInt();
 		
-		if(N==4){
+		
 		    System.out.println("yes");
-		}
-		else{
-		    System.out.println("no");
-		}
 
 	}
 }
