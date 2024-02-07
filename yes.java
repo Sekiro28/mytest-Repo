9828662660ef7e57999788681e1f7293613c692e
Harshit <108220156+Sekiro28@users.noreply.github.com>
2024-02-07 08:50:01 +0530

    Create yes.java

---
+++ yes.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner read = new Scanner(System.in);
+		int N = read.nextInt();
+		
+		if(N==4){
+		    System.out.println("yes");
+		}
+		else{
+		    System.out.println("no");
+		}
+
+	}
+}
